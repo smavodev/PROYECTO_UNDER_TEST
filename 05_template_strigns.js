b69880700af63e7a9a6730d1808afc1476db4111
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2024-06-14 18:15:29 -0500

    Add template_string, number, boolean, undefined

---
+++ 05_template_strigns.js
@@ -0,0 +1,15 @@
+// **********  4. Template Strings -  **********
+
+let nombre = 'Sergio'
+let cargo = 'QA AUTOMATION'
+let empresa = 'OKA'
+
+let persona0 = nombre + cargo + empresa
+console.log(persona0)
+
+let persona = "Hola soy: " + nombre + " y soy " + cargo + " y trabajo en " + empresa;
+console.log(persona)
+
+let empleado = `Hola soy ${nombre} y tengo el cargo de ${cargo} y actualmente trabajo en ${empresa}`
+console.log(empleado)
+
---
+++ 08_undefined.js
@@ -0,0 +1,31 @@
+  // **********  7. undefined, null & NaN  **********
+  
+    // undefined indica que no se ha inicializado una variable y que el valor está ausente
+    let indefinida ;
+    console.log(indefinida);
+
+    //null es un valor especial que indica la ausencia de un valor
+    let nulo = null;
+    console.log(null);
+
+    //NaN - Not a Number
+    let noEsUnNumero = "hola" * 3.7;
+    console.log(noEsUnNumero);
+
+    let Mayuscula = null;
+    let C = 7.19;
+    console.log(C + Number.parseInt(Mayuscula));
+
+
+
+    // Playwright
+    // Cypress
+    // Selenium
+    // testcafe
+
+    // funciones
+    // arrays
+    // objetos 
+    // tipos de operadores
+    // condicionales
+    // ciclos
