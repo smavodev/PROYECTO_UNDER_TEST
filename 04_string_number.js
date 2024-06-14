64268b6a8748a00fb6eef2d29737c8cf9da4decf
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2024-06-13 20:19:41 -0500

    Add string_numer.js

---
+++ 04_string_number.js
@@ -0,0 +1,20 @@
+
+// **********   3. Cadenas de Texto (Strings) y numeros (Numbers) **********
+
+let nombre = '3'
+console.log(nombre)
+let numero1 = 3
+let numero2 = 5;
+console.log(numero1)
+
+let suma1 = nombre + numero1  // Concatenar
+let suma2 = numero2 + numero1; // Sumatoria de numeros
+
+console.log(suma1)  // Johan3
+console.log(suma2); // 8
+
+console.log(nombre.lengt) //
+console.log(nombre.toLocaleLowerCase()) //
+console.log(nombre.toLocaleUpperCase()) //
+
+console.log(numero1.toString())
